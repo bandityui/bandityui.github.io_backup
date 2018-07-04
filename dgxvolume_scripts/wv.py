10e1c1fbabd1c9f80e808fded7ba5d14ed41aa8f
bandityui <northeyt@live.ie>
2018-07-04 21:23:14 +0000

    .

--- dgxvolume_scripts/wv.py
+++ dgxvolume_scripts/wv.py
@@ -24,53 +24,50 @@ day  = 24*hour
 week = 7*day
 quarter = 90*day
 
-# seconds from beginning
-d0 = datetime.datetime(2018, 3, 23, 8, 58)
-delta = now - d0
-ts_now = delta.total_seconds()
-t0 = amount[0]['timeStamp']  # time (s) of first tx 
-tlast = amount[length-1]['timeStamp']  # time (s) of first tx 
-a = int(ts_now) - (int(tlast) - int(t0))
-with open('tsince_lasttx.txt','w+') as f:
-  f.write(str(a))
 
 # Begin weekly table
 print("### Weekly volume table\n")
 print("Week Starting | Volume (DGX)")
 print("--- | ---")
 
-with open('weekly.dat','w+') as f:
+t0 = amount[0]['timeStamp'] 		        # time (s) of first tx 
+tlast = amount[length-1]['timeStamp']           # time (s) of last tx 
+dt = int(tlast) - int(t0)			# time between 1st and last txs
+d0 = now - datetime.timedelta(seconds=dt)	# current time minus dt
+di = d0
+with open('weekly.dat','w+') as f:		# open file for writing
   wv = 0                                        # weekly volume 
   tv = 0                                        # total volume
   cw = 1                                        # week counter (start at 1)
+  mintv = 0					# total supply
   t0 = amount[0]['timeStamp']                   # time of first tx
   for i in range(0,length):
-    x = int(amount[i]['value'])  # volume for ith tx
+    x = int(amount[i]['value'])  		# volume for ith tx
+    tv = tv + x					# accumulate total volume
+    ti = amount[i]['timeStamp']        	        # time of ith tx
+    dt = int(ti) - int(t0)                      # seconds since t0
     if amount[i]['from'] == '0x0000000000000000000000000000000000000000':  # if from 0x0 (minting)
       mintv = mintv + x  # DGX minted 
     elif amount[i]['to'] == '0x0000000000000000000000000000000000000000':  # if to 0x0 (recasting)
       wv = wv - x  # these are recasting txs
       mintv = mintv - x  
     else:					# else is a normal tx
-      ti = amount[i]['timeStamp']               # time of ith tx
-      dt = int(ti) - int(t0)                    # seconds since t0
       vi = int(amount[i]['value'])              # volume of ith tx
       wv = wv + vi                              # accumulate weekly volume
       tv = tv + vi                              # accumulate total volume
     if dt >= cw*week:                           # if dt > cw weeks
       cw += 1                                   # +1 week
-      di = d0 + datetime.timedelta(seconds=dt)  # datetime of ith tx
       y = round(float(wv)/1e9,2)                # round
       y2 = round(float(mintv)/1e9,2)
       print(str(di.strftime("%d/%m/%Y")) + "|" + str(y))  # print information to file
       f.write(str(di) + ' ' + str(y) + ' ' + str(y2) + '\n')    # write week number, volume to file
+      di = d0 + datetime.timedelta(seconds=dt)  # datetime of ith tx
       wv = 0                                    # reset weekly volume 
-wv = round(float(wv)/1e9,2)                     # current, unfinished week
-tv = round(float(tv)/1e9,2)                     # round
-y2 = round(float(mintv)/1e9,2)
-
-f.write(str(datew) + ' ' + str(wv) + ' ' + str(y2) + '\n')  # write week number, volume to file
-print(str(datew.strftime("%d/%m/%Y")) + "|" + str(wv))
+  wv = round(float(wv)/1e9,2)                     # current, unfinished week
+  tv = round(float(tv)/1e9,2)                     # round
+  y2 = round(float(mintv)/1e9,2)
+  f.write(str(di) + ' ' + str(wv) + ' ' + str(y2) + '\n')  # write week number, volume to file
+print(str(di.strftime("%d/%m/%Y")) + "|" + str(wv))
 
 # All-time volume table
 #print("Current Quarter |" + str(dateq.strftime("%d/%m/%Y")) + "|" + str(qv) )
@@ -84,6 +81,6 @@ print(str(d0.strftime("%d/%m/%Y"))    + "|" + str(tv) + "\n")
 print("### Total Supply\n")
 print("| DGX Total Supply |")
 print("| --- |")
-print("|" + y2 + "|\n")
+print("|" + str(y2) + "|\n")
 
 
--- update_dgxvolume.sh
+++ update_dgxvolume.sh
@@ -2,7 +2,7 @@
 
 cp /home/thomas/bandityui.github.io/_drafts/dgxvolume.template /home/thomas/bandityui.github.io/dgxvolume.markdown
 cd /home/thomas/bandityui.github.io/dgxvolume_scripts 
-python3 weekly_volume.py >> ../dgxvolume.markdown
+python3 wv.py >> ../dgxvolume.markdown
 gnuplot graph_weekly.gp
 ./eps2png.sh out.eps
 A=$(cat date.txt)

1b21b37e9aaf1a7098e904fbd529a1aa47509e0d
bandityui <northeyt@live.ie>
2018-07-04 22:43:03 +0000

    .

--- dgxvolume_scripts/wv.py
+++ dgxvolume_scripts/wv.py
@@ -29,7 +29,7 @@ print("### Weekly volume table\n")
 print("Week Starting | Volume (DGX)")
 print("--- | ---")
 
-t0 = amount[0]['timeStamp'] 		        # time (s) of first tx 
+t0 = int(amount[0]['timeStamp'])	        # time (s) of first tx 
 tlast = amount[length-1]['timeStamp']           # time (s) of last tx 
 dt = int(tlast) - int(t0)			# time between 1st and last txs
 d0 = now - datetime.timedelta(seconds=dt)	# current time minus dt
@@ -38,31 +38,31 @@ with open('weekly.dat','w+') as f:		# open file for writing
   wv = 0                                        # weekly volume 
   tv = 0                                        # total volume
   cw = 1                                        # week counter (start at 1)
-  mintv = 0					# total supply
-  t0 = amount[0]['timeStamp']                   # time of first tx
+  ts = 0					# total supply
   for i in range(0,length):
-    vi = int(amount[i]['value'])              # volume of ith tx
-    ti = amount[i]['timeStamp']        	        # time of ith tx
-    dt = int(ti) - int(t0)                      # seconds since t0
+    vi = int(amount[i]['value'])                # volume of ith tx
+    ti = int(amount[i]['timeStamp'])	        # time of ith tx
+    dt = ti - t0	                        # seconds since t0
     if amount[i]['from'] == '0x0000000000000000000000000000000000000000':  # if from 0x0 (minting)
-      mintv = mintv + vi  # DGX minted 
+      ts = ts + vi  # DGX minted 
     elif amount[i]['to'] == '0x0000000000000000000000000000000000000000':  # if to 0x0 (recasting)
       wv = wv - vi  # these are recasting txs
-      mintv = mintv - vi  
+      tv = tv - vi
+      ts = ts - vi  
     else:					# else is a normal tx
       wv = wv + vi                              # accumulate weekly volume
       tv = tv + vi                              # accumulate total volume
-    if dt >= cw*week:                           # if dt > cw weeks
+    if dt > cw*week:                            # if dt > cw weeks
       cw += 1                                   # +1 week
       y = round(float(wv)/1e9,2)                # round
-      y2 = round(float(mintv)/1e9,2)
+      y2 = round(float(ts)/1e9,2)
       print(str(di.strftime("%d/%m/%Y")) + "|" + str(y))  # print information to file
       f.write(str(di) + ' ' + str(y) + ' ' + str(y2) + '\n')    # write week number, volume to file
       di = d0 + datetime.timedelta(seconds=dt)  # datetime of ith tx
       wv = 0                                    # reset weekly volume 
   wv = round(float(wv)/1e9,2)                     # current, unfinished week
   tv = round(float(tv)/1e9,2)                     # round
-  y2 = round(float(mintv)/1e9,2)
+  y2 = round(float(ts)/1e9,2)
   f.write(str(di) + ' ' + str(wv) + ' ' + str(y2) + '\n')  # write week number, volume to file
 print(str(di.strftime("%d/%m/%Y")) + "|" + str(wv))
 

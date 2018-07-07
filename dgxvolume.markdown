d78d79457609e8e1fc6bba6652a19aac2b77057c
bandityui <northeyt@live.ie>
2018-07-07 13:18:11 +0000

    .

--- dgxvolume.markdown
+++ dgxvolume.markdown
@@ -1,7 +1,7 @@
 ---
 layout: post
 title:  "DGX on-chain volume"
-date:   2018-07-07 13:00:01.624366
+date:   2018-07-07 13:18:00.368985
 categories: DGX
 ---
 
@@ -14,27 +14,27 @@ categories: DGX
 ![DGX weekly volume graph](dgxvolume_scripts/out.png)
 
 This page updates hourly using data from the [DGX contract address (etherscan)](https://etherscan.io/token/0x4f3afec4e5a3f2a6a1a411def7d7dfe50ee057bf). Last updated:
-2018-07-07 13:00 UTC
+2018-07-07 13:18 UTC
 
 ### Weekly volume table
 
 Week Starting | Volume (DGX)
 --- | ---
-23/03/2018|14.35
-01/04/2018|25.77
-06/04/2018|2823.35
-13/04/2018|1931.19
-20/04/2018|2172.83
-28/04/2018|10249.51
-04/05/2018|32607.32
+23/03/2018|9.35
+02/04/2018|19.42
+06/04/2018|362.53
+13/04/2018|906.31
+20/04/2018|625.95
+28/04/2018|4534.97
+04/05/2018|32405.32
 11/05/2018|15414.93
 18/05/2018|9175.26
-25/05/2018|23539.97
+25/05/2018|23530.28
 01/06/2018|9161.14
 08/06/2018|12041.46
 15/06/2018|1384.45
-22/06/2018|4931.62
-29/06/2018|4403.68
+22/06/2018|4930.51
+29/06/2018|4402.69
 06/07/2018|10.54
 
 
@@ -42,7 +42,7 @@ Week Starting | Volume (DGX)
 
 | All-time volume (DGX) |
 | --- |
-|129887.36|
+|118915.11|
 
 ### Total transaction fees collected
 
--- dgxvolume_scripts/wv.py
+++ dgxvolume_scripts/wv.py
@@ -2,6 +2,72 @@ import urllib.request
 import json
 import datetime
 
+def xvolume(x,printtable):
+  '''
+  xvolume: writes datetime and volume to .dat file every 'x' time-period (seconds) 
+  '''
+  
+  xv = 0                                        # volume in time period 'x'
+  vdigix = 0					# volume from digix marketplace
+  tv = 0                                        # total volume
+  tx = 0                                        # tx fees collected
+  cx = 1                                        # count time periods (start at 1)
+  ts = 0				       	# total supply
+  di = d0 					# start on d0
+
+  with open(str(x) + '.dat','w+') as f:		# open file for writing
+    for i in range(0,length):
+      vi = int(amount[i]['value'])              # volume of ith tx
+      ti = int(amount[i]['timeStamp'])	        # time of ith tx
+      dt = ti - t0	                        # seconds since t0
+      ato = amount[i]['to']			# to of ith tx
+      afrom = amount[i]['from']			# from of ith tx
+      if i > 0:
+        afrom1 = amount[i-1]['from']		# from of (i-1)th tx
+      if afrom == '0x0000000000000000000000000000000000000000':  # if from 0x0 (minting)
+        ts = ts + vi  # Minting increases total supply
+      elif ato == '0x0000000000000000000000000000000000000000':  # if to 0x0 (recasting)
+        ts = ts - vi  # Recasting decreases total supply
+      elif ato == '0x26cab6888d95cf4a1b32bd37d4091aa0e29e7f68':  # recast fee collector
+        pass
+      elif ato == '0x00a55973720245819ec59c716b7537dac5ed4617':  # tx fee collector
+        tx = tx + vi
+        #elif amount[i]['to'] == '0x964f35fae36d75b1e72770e244f6595b68508cf5':  # kyber contract 
+        #print('kyber')
+        '''
+        When e.g. 10 DGX is sent 3 tx occur: first 0.013 (the tx fee), then 9.987, lastly 10. 
+        This conditional avoids counting the 9.987 because the 0.013 and the 9.987 have the same 'from' address.
+        '''
+      elif afrom == afrom1:
+        continue
+      #elif afrom == '0xd5be9efcc0fbea9b68fa8d1af641162bc92e83f2':  #  from digix marketplace
+      #  vdigix = vdigix + vi			# accumulate volume from digix marketplace
+      #  xv = xv + vi                            # accumulate 'x'ly volume
+      #  tv = tv + vi                            # accumulate total volume
+      else:					# else is a normal tx
+        xv = xv + vi                            # accumulate 'x'ly volume
+        tv = tv + vi                            # accumulate total volume
+      if dt > cx*x:                             # if dt > cx multiples of x time periods
+        cx = int(dt/x) + 1                      # amount of x time periods passed
+        y1 = round(float(xv)/1e9,2)             # round
+        if printtable == 1:
+          print(str(di.strftime("%d/%m/%Y")) + "|" + str(y1))  # print information
+        y2 = round(float(ts)/1e9,2)
+        f.write(str(di) + ' ' + str(y1) + ' ' + str(y2) + '\n')    # write date, volume to file
+        di = d0 + datetime.timedelta(seconds=dt)  # datetime of ith tx
+        xv = 0                                    # reset x volume 
+    xv = round(float(xv)/1e9,2)                   # current, unfinished week
+    tv = round(float(tv)/1e9,2)
+    ts = round(float(ts)/1e9,2)
+    tx = round(float(tx)/1e9,2)
+    vdigix = round(float(vdigix)/1e9,2)
+    f.write(str(di) + ' ' + str(xv) + ' ' + str(ts) + '\n')    	   # write date, volume to file
+  if printtable == 1:
+    print(str(di.strftime("%d/%m/%Y")) + "|" + str(xv))
+  
+  return xv,tv,ts,tx
+
+
 now = datetime.datetime.now()
 with open('date.txt','w+') as f:
   f.write(str(now))
@@ -22,69 +88,18 @@ day  = 24*hour
 week = 7*day
 quarter = 90*day
 
-# Begin weekly table
-print("### Weekly volume table\n")
-print("Week Starting | Volume (DGX)")
-print("--- | ---")
-
 t0 = int(amount[0]['timeStamp'])	        # time (s) of first tx 
 tlast = amount[length-1]['timeStamp']           # time (s) of last tx 
 dt = int(tlast) - int(t0)			# time between 1st and last txs
 d0 = now - datetime.timedelta(seconds=dt)	# current time minus dt
 di = d0
-with open('weekly.dat','w+') as f:		# open file for writing
-  wv = 0                                        # weekly volume 
-  vdigix = 0					# volume from digix marketplace
-  tv = 0                                        # total volume
-  cw = 1                                        # week counter (start at 1)
-  ts = 0					# total supply
-  tx = 0					# tx fees
-  for i in range(0,length):
-    vi = int(amount[i]['value'])                # volume of ith tx
-    ti = int(amount[i]['timeStamp'])	        # time of ith tx
-    dt = ti - t0	                        # seconds since t0
-    ato = amount[i]['to']			# to of ith tx
-    afrom = amount[i]['from']			# from of ith tx
-    if i > 0:
-      afrom1 = amount[i-1]['from']		# from of (i-1)th tx
-    if afrom == '0x0000000000000000000000000000000000000000':  # if from 0x0 (minting)
-      ts = ts + vi  # Minting increases total supply
-    elif ato == '0x0000000000000000000000000000000000000000':  # if to 0x0 (recasting)
-      ts = ts - vi  # Recasting decreases total supply
-    elif ato == '0x26cab6888d95cf4a1b32bd37d4091aa0e29e7f68':  # recast fee collector
-      pass
-    elif ato == '0x00a55973720245819ec59c716b7537dac5ed4617':  # tx fee collector
-      tx = tx + vi
-      #elif amount[i]['to'] == '0x964f35fae36d75b1e72770e244f6595b68508cf5':  # kyber contract 
-      #print('kyber')
-    elif afrom == '0xd5be9efcc0fbea9b68fa8d1af641162bc92e83f2':  #  from digix marketplace
-      vdigix = vdigix + vi
-      wv = wv + vi                              # accumulate normal weekly volume
-      tv = tv + vi                              # accumulate total volume
-      '''
-      When e.g. 10 DGX is sent 3 tx occur: first 0.013 (the tx fee), then 9.987, lastly 10. 
-      This conditional avoids counting the 9.987 because the 0.013 and the 9.987 have the same 'from' address.
-      '''
-    elif afrom == afrom1:
-      continue
-    else:					# else is a normal tx
-      wv = wv + vi                              # accumulate normal weekly volume
-      tv = tv + vi                              # accumulate total volume
-    if dt > cw*week:                            # if dt > cw weeks
-      cw += 1                                   # +1 week
-      y1 = round(float(wv)/1e9,2)                # round
-      print(str(di.strftime("%d/%m/%Y")) + "|" + str(y1))  # print information
-      y2 = round(float(ts)/1e9,2)
-      y3 = round(float(vdigix)/1e9,2)
-      f.write(str(di) + ' ' + str(y1) + ' ' + str(y3) + ' ' + str(y2) + '\n')    # write week number, volume to file
-      di = d0 + datetime.timedelta(seconds=dt)  # datetime of ith tx
-      wv = 0                                    # reset weekly volume 
-      vdigix = 0                                # reset
-  y1 = round(float(wv)/1e9,2)                	# round
-  y2 = round(float(ts)/1e9,2)
-  y3 = round(float(vdigix)/1e9,2)
-  f.write(str(di) + ' ' + str(y1) + ' ' + str(y3) + ' ' + str(y2) + '\n')    # write week number, volume to file
-print(str(di.strftime("%d/%m/%Y")) + "|" + str(y1))
+
+# Begin weekly table
+print("### Weekly volume table\n")
+print("Week Starting | Volume (DGX)")
+print("--- | ---")
+
+wv,tv,ts,tx = xvolume(week,1)
 
 # All-time volume table
 #print("Current Quarter |" + str(dateq.strftime("%d/%m/%Y")) + "|" + str(qv) )
@@ -92,100 +107,17 @@ print("\n")
 print("### All-time volume\n")
 print("| All-time volume (DGX) |")
 print("| --- |")
-tv = round(float(tv)/1e9,2)
 print("|" + str(tv) + "|\n")
 
 print("### Total transaction fees collected\n")
 print("| Transaction fees (DGX) |")
 print("| --- |")
-tx = round(float(tx)/1e9,2)                     # current, unfinished week
 print("|" + str(tx) + "|\n")
 
 print("### Total Supply\n")
 print("| DGX Total Supply |")
 print("| --- |")
-ts = round(float(ts)/1e9,2)
 print("|" + str(ts) + "|\n")
 
-def xvolume(x):
-  '''
-  xvolume: writes datetime and volume to .dat file every 'x' time-period (seconds) 
-  '''
-  
-  xv = 0                                        # volume in time period 'x'
-  tv = 0                                        # total volume
-  cx = 1                                        # count time periods (start at 1)
-  ts = 0				       	# total supply
-  di = d0 					# start on d0
-  vdigix = 0					# volume from digix marketplace
+wv,tv,ts,tx = xvolume(day,0)
 
-  with open(str(x) + '.dat','w+') as f:		# open file for writing
-    for i in range(0,length):
-      vi = int(amount[i]['value'])              # volume of ith tx
-      ti = int(amount[i]['timeStamp'])	        # time of ith tx
-      dt = ti - t0	                        # seconds since t0
-      ato = amount[i]['to']			# to of ith tx
-      afrom = amount[i]['from']			# from of ith tx
-      if i > 0:
-        afrom1 = amount[i-1]['from']		# from of (i-1)th tx
-      if afrom == '0x0000000000000000000000000000000000000000':  # if from 0x0 (minting)
-        ts = ts + vi  # Minting increases total supply
-      elif afrom == '0xd5be9efcc0fbea9b68fa8d1af641162bc92e83f2':  #  from digix marketplace
-        pass
-      elif ato == '0x0000000000000000000000000000000000000000':  # if to 0x0 (recasting)
-        ts = ts - vi  # Recasting decreases total supply
-      elif ato == '0x26cab6888d95cf4a1b32bd37d4091aa0e29e7f68':  # recast fee collector
-        pass
-      elif ato == '0x00a55973720245819ec59c716b7537dac5ed4617':  # tx fee collector
-        pass
-        #elif amount[i]['to'] == '0x964f35fae36d75b1e72770e244f6595b68508cf5':  # kyber contract 
-        #print('kyber')
-        '''
-        When e.g. 10 DGX is sent 3 tx occur: first 0.013 (the tx fee), then 9.987, lastly 10. 
-        This conditional avoids counting the 9.987 because the 0.013 and the 9.987 have the same 'from' address.
-        '''
-      elif afrom == afrom1:
-        continue
-      elif afrom == '0xd5be9efcc0fbea9b68fa8d1af641162bc92e83f2':  #  from digix marketplace
-        vdigix = vdigix + vi
-        xv = xv + vi                            # accumulate weekly volume
-      else:					# else is a normal tx
-        xv = xv + vi                            # accumulate weekly volume
-      if dt > cx*x:                             # if dt > cx multiples of x time periods
-        cx = int(dt/x) + 1                      # amount of x time periods passed
-        y1 = round(float(dv)/1e9,2)             # round
-        print(str(di.strftime("%d/%m/%Y")) + "|" + str(y1))  # print information
-        y2 = round(float(ts)/1e9,2)
-        y3 = round(float(vdigix)/1e9,2)
-        f.write(str(di) + ' ' + str(y1) + ' ' + str(y3) + ' ' + str(y2) + '\n')    # write date, volume to file
-        di = d0 + datetime.timedelta(seconds=dt)  # datetime of ith tx
-        dv = 0                                    # reset weekly volume 
-    y1 = round(float(y1)/1e9,2)                   # current, unfinished week
-    y2 = round(float(ts)/1e9,2)
-    y3 = round(float(vdigix)/1e9,2)
-    f.write(str(di) + ' ' + str(y1) + ' ' + str(y3) + ' ' + str(y2) + '\n')    	   # write date, volume to file
-  print(str(di.strftime("%d/%m/%Y")) + "|" + str(y1))
-  
-  # All-time volume table
-  #print("Current Quarter |" + str(dateq.strftime("%d/%m/%Y")) + "|" + str(qv) )
-  print("\n")
-  print("### All-time volume\n")
-  print("| All-time volume (DGX) |")
-  print("| --- |")
-  tv = round(float(tv)/1e9,2)
-  print("|" + str(tv) + "|\n")
-  
-  print("### Total transaction fees collected\n")
-  print("| Transaction fees (DGX) |")
-  print("| --- |")
-  tx = round(float(tx)/1e9,2)
-  print("|" + str(tx) + "|\n")
-  
-  print("### Total Supply\n")
-  print("| DGX Total Supply |")
-  print("| --- |")
-  ts = round(float(ts)/1e9,2)
-  print("|" + str(ts) + "|\n")
-  return
-
-xvolume()
